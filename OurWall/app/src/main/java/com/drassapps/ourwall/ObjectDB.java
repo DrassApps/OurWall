b70d2eae5ff03ba6450a48214242a06c52c8a9bf
Andrés <drass.apps@gmail.com>
2018-06-04 19:31:56 +0200

    Class object to ref user data db

---
+++ OurWall/app/src/main/java/com/drassapps/ourwall/ObjectDB.java
@@ -0,0 +1,15 @@
+package com.drassapps.ourwall;
+
+public class ObjectDB {
+
+    public String lat, lon, input, email;
+
+    public ObjectDB() {}
+
+    public ObjectDB(String lat, String lon, String input, String email) {
+        this.lat = lat;
+        this.lon = lon;
+        this.input = input;
+        this.email = email;
+    }
+}
