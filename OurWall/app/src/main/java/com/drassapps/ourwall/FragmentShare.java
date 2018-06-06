56c4824870f482e1011aab01528aa933304571d0
Andrés <drass.apps@gmail.com>
2018-06-06 12:03:04 +0200

    New share available text, sound and images

--- OurWall/app/src/main/java/com/drassapps/ourwall/FragmentShare.java
+++ OurWall/app/src/main/java/com/drassapps/ourwall/FragmentShare.java
@@ -8,7 +8,8 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.TextInputLayout;
+import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,7 +18,16 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -26,19 +36,27 @@ import java.util.Date;
 
 public class FragmentShare extends Fragment {
 
-    private RelativeLayout photo, text, sound, mLayout, share;
-    public static final int PICK_IMAGE = 1;
-    public static final int PICK_SOUND = 2;
+    // MARK - CLASS
 
-    private Bitmap selectedImage;
+    private Common ui = new Common();
+    private SharedPreferences.Editor editor;
+
+    // MARK - PROPERTIES
+
+    private static final int PICK_IMAGE = 1;
+    private static final int PICK_SOUND = 2;
     private Uri imageUri, soundUri;
+    private String type, currentUserEmail, currentRef, lat, lon;
+    private String nMessagesSend, nImagesSend, nSoundSend;
 
+    // MARK - UI
+
+    private RelativeLayout photo, text, sound, mLayout, share;
     private TextView infoText, dataText;
     private EditText textToShre;
-    private Common ui = new Common();
-
-    private String type;
     private ImageView imagePicked;
+
+    // MARK - MAIN
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle s) {
@@ -48,6 +66,17 @@ public class FragmentShare extends Fragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+        editor = sharedPref.edit();
+        nMessagesSend = sharedPref.getString("nMessagesSend", "");
+        nImagesSend = sharedPref.getString("nImagesSend", "");
+        nSoundSend = sharedPref.getString("nSoundSend", "");
+        currentRef = sharedPref.getString("currentRef", "");
+        currentUserEmail = sharedPref.getString("userEmail", "");
+
+        lon = sharedPref.getString("lon", "");
+        lat = sharedPref.getString("lat", "");
 
         photo = getView().findViewById(R.id.share_photoLay);
         text = getView().findViewById(R.id.share_textLay);
@@ -88,27 +117,31 @@ public class FragmentShare extends Fragment {
         share.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // TODO: USAR RETROFIT PARA SUBIR LOS DATOS AL SERVIDOR Y ACTUALIZAR EL MAPA
                 switch (type){
                     case "image":
+                        uploadFile(imageUri);
+                        updateCount("image");
                         break;
 
                     case "sound":
+                        uploadFile(soundUri);
+                        updateCount("sound");
                         break;
 
                     case "text":
-                        textToShre.getText().toString();
-                        if (textToShre.length() > 5) {
-
-                        }else {
-                            ui.setSnackBar(mLayout,"Introduce un texto más largo");
+                        String userText = textToShre.getText().toString();
+                        if (userText.length() > 5) {
+                            uploadText(userText);
+                            updateCount("text");
                         }
+                        else { ui.setSnackBar(mLayout,getString(R.string.err_lenght)); }
                         break;
                 }
             }
         });
     }
 
+    // Update ui for upload a file
     private void updateUI(String type) {
         photo.setVisibility(View.GONE);
         text.setVisibility(View.GONE);
@@ -117,77 +150,107 @@ public class FragmentShare extends Fragment {
         infoText.setVisibility(View.VISIBLE);
         dataText.setVisibility(View.VISIBLE);
 
-        SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
-        String lon = sharedPref.getString("lon", "");
-        String lat = sharedPref.getString("lat", "");
-
         String lastUpdateTime = DateFormat.getTimeInstance().format(new Date());
 
         dataText.setText("Longitud: "+lon+"\n"+"Latitud: "+lat+"\n"+"Fecha: "+lastUpdateTime);
 
         switch (type){
             case "image":
-                infoText.setText("Vas a añadir una imagen");
+                infoText.setText(getString(R.string.shareView_image));
                 break;
 
             case "sound":
-                infoText.setText("Vas a añadir un sonudi");
+                infoText.setText(getString(R.string.shareView_sound));
                 break;
 
             case "text":
                 textToShre.setVisibility(View.VISIBLE);
-                infoText.setText("Vas a añadir un texto");
+                infoText.setText(getString(R.string.shareView_text));
                 break;
         }
     }
 
-    // MARK - GET DATA FROM DEVICE
+    // MARK - METHODS
     private void getResourceFromDevice(String type, int pick) {
-        Intent intent = new Intent();
-        intent.setType(type+"/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent, "Elige una"), pick);
+        Intent i = new Intent();
+        i.setType(type+"/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(i,getString(R.string.shareView_choose)),pick);
     }
+
+    // Upload file
+    private void uploadFile(Uri uri) {
+        StorageReference ref = FirebaseStorage.getInstance().getReference().child(currentRef);
+
+        UploadTask uploadTask = ref.putFile(uri);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                ui.setSnackBar(mLayout,getString(R.string.shareView_err_upload));
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                ui.setSnackBar(mLayout,getString(R.string.shareView_suc_upload));
+                uploadText("");
+            }
+        });
+    }
+
+    // Upload text and update primary key
+    private void uploadText(String text) {
+        DatabaseReference mDatabaseRef = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference mDatabaseRefUser = mDatabaseRef.child(currentRef);
+        ObjectDB objectDB = new ObjectDB(lat,lon,text,currentUserEmail);
+        mDatabaseRefUser.setValue(objectDB);
+        updateRef();
+    }
+
+    private void updateCount(String type) {
+        switch (type) {
+            case "image":
+                editor.putString("nImagesSend", String.valueOf(Integer.valueOf(nImagesSend)+1));
+                break;
+
+            case "sound":
+                editor.putString("nSoundSend", String.valueOf(Integer.valueOf(nSoundSend)+1));
+
+                break;
+
+            case "text":
+                editor.putString("nMessagesSend",String.valueOf(Integer.valueOf(nMessagesSend)+1));
+                break;
+        }
+    }
+
+    // Current ref will be primary key on our db and storage
+    private void updateRef() {
+        editor.putString("currentRef", String.valueOf(Integer.parseInt(currentRef)+1));
+        editor.commit();
+    }
+
+    // MARK - RESTUL
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        // Si ha elegido una imagen
         if (requestCode == PICK_IMAGE) {
             try {
-                // Si tenemos datos
                 if (data != null) {
-
-                    // Obtenemos una URI de la ubicación de la imagen
                     imageUri = data.getData();
+                    InputStream imageStream = getActivity().
+                            getContentResolver().openInputStream(imageUri);
 
-                    // Abrimos la URI a través de un InputStram
-                    final InputStream imageStream = getActivity()
-                            .getContentResolver().openInputStream(imageUri);
-
-                    // Convertimos ese inputstream a un BitMap
-                    selectedImage = BitmapFactory.decodeStream(imageStream);
-
-                    // La asignamos a la ImagevIEW
+                    Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
                     imagePicked.setImageBitmap(selectedImage);
                     imagePicked.setVisibility(View.VISIBLE);
 
-                    // Si no hay data significa que el usuario no ha elegido ninguna imagen
-                } else {
-                    ui.setSnackBar(mLayout, "meh");
-                }
+                } else { ui.setSnackBar(mLayout, getString(R.string.err_getData)); }
 
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            } catch (IOException e) { e.printStackTrace(); }
         }
 
-        // Si ha elegido una cacion
         if (requestCode == PICK_SOUND) {
-            // Si tenemos datos
             if (data != null) {
-
-                // Obtenemos una URI de la ubicación del sonido
                 soundUri = data.getData();
-
             }
         }
     }
